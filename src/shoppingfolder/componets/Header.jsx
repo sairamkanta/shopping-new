f16de604fa3fad9bc8dd1b64bb2f59ea7eae7444
sairam8639 <sairamkanta9@gmail.com>
2024-07-28 20:06:57 +0530

    changes

--- src/shoppingfolder/componets/Header.jsx
+++ src/shoppingfolder/componets/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                 Signin / Signup
             </div>
             <div className="cart">
-                Cart one
+                Cart 
             </div>
         </div>
     </div>
