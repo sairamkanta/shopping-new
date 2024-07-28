992b3751feef5ea9806dbdfe06b3e20ecf9d1563
sairam8639 <sairamkanta9@gmail.com>
2024-07-28 19:40:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import React from 'react'
+
+import './App.css'
+import Mainpage from './shoppingfolder/pages/Mainpage'
+
+const App = () => {
+  return (
+    <div>
+      <h1>React shopping</h1>
+      <Mainpage/>
+       </div>
+  )
+}
+
+export default App
---
+++ src/shoppingfolder/pages/Mainpage.jsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+import Header from '../componets/Header'
+import Banner from '../componets/Banner'
+import Collection from '../componets/Collection'
+import Footer from '../componets/Footer'
+
+const Mainpage = () => {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Collection/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Mainpage
