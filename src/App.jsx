1522010d5adcfd944c6c97214e1c66849f0f6ea7
sairam8639 <sairamkanta9@gmail.com>
2024-07-31 13:12:09 +0530

    31/07/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 
-
-
 import React from 'react'
-
 import './App.css'
 import Mainpage from './shoppingfolder/pages/Mainpage'
 
 const App = () => {
   return (
     <div>
-     
       <Mainpage/>
        </div>
   )
--- src/shoppingfolder/componets/Banner.jsx
+++ src/shoppingfolder/componets/Banner.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='bannersection'>
+      <div className="bannerbox">
+        <img src="https://shopify-ecommerce-university.s3.amazonaws.com/production/images/success_story/banner_image/699/mens_fashion_shopify_success_stories_Banner.jpg  " alt="banner" width="100%" height="50%"/>
+      </div>
+    </div>
   )
 }
 
