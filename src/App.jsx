88aeb9dc7746243d2c5c658e96ae9746e913d599
sairam8639 <sairamkanta9@gmail.com>
2024-07-31 22:45:01 +0530

    navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Mainpage from './shoppingfolder/pages/Mainpage'
 
--- src/shoppingfolder/componets/Header.jsx
+++ src/shoppingfolder/componets/Header.jsx
@@ -1,34 +1,39 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
-    <div className='HeaderSection'>
-        <div className="left">
-            <div className="title">
-                <h2>Shopping Mall</h2>
-            </div>
-        </div>
-        <div className="center">
-            <ul>
-                <li>Women</li>
-                <li>Men</li>
-                <li>Children</li>
-                <li>Beauty</li>
-            </ul>
-
-        </div>
-        <div className="search">
-            <input type="text" placeholder='search....'/>
-        </div>
-        <div className="right">
-            <div className="signin">
-                Signin / Signup
-            </div>
-            <div className="cart">
-                Cart 
-            </div>
-        </div>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container >
+      <Navbar.Brand href="#" className='fw-bold'>STYLES & FASHION</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="mx-auto my-2 my-lg-0 fw-bold" 
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+          <Nav.Link href="#action1">WOMEN</Nav.Link>
+          <Nav.Link href="#action2">MEN</Nav.Link>
+           <Nav.Link href="#action2">CHILDREN</Nav.Link>
+           <Nav.Link href="#action2">BEAUTY</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
